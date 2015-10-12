827df1b15413ef4ec9ee0a3c25cfd3f297e34091
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-12 14:08:37 +0200

    hateoas lib added

--- src/main/java/pl/java/scalatech/BootRest.java
+++ src/main/java/pl/java/scalatech/BootRest.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataTestApplication {
+public class BootRest {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataTestApplication.class, args);
+        SpringApplication.run(BootRest.class, args);
     }
 }
