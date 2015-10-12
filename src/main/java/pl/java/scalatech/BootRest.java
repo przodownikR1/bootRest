4c3b7003f1e998961431c9f2b975742adcfc1b29
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-12 14:39:05 +0200

    create user and start boot main

--- src/main/java/pl/java/scalatech/BootRest.java
+++ src/main/java/pl/java/scalatech/BootRest.java
@@ -1,11 +1,30 @@
 package pl.java.scalatech;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.google.common.collect.Lists;
+
+import lombok.extern.slf4j.Slf4j;
+import pl.java.scalatech.domain.User;
+import pl.java.scalatech.repository.UserRepository;
 
 @SpringBootApplication
+@Slf4j
 public class BootRest {
 
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return (evt) ->
+                Lists.newArrayList("jhoeller","dsyer","pwebb","ogierke","rwinch","mfisher","mpollack","jlong")
+                        .forEach(a -> {
+                            User user = userRepository.save(User.builder().email("przodownik@tlen.pl").login(a).build());
+                            log.info("{}",user);
+                        });
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(BootRest.class, args);
     }
--- src/main/java/pl/java/scalatech/domain/User.java
+++ src/main/java/pl/java/scalatech/domain/User.java
@@ -5,9 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
 
     @Id
