68be659063a379137758402657f4b769fdaa329d
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-15 15:50:45 +0200

    first simple spock rest test

--- src/main/java/pl/java/scalatech/web/example/SampleRestController.java
+++ src/main/java/pl/java/scalatech/web/example/SampleRestController.java
@@ -1,5 +1,6 @@
 package pl.java.scalatech.web.example;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class SampleRestController {
 
     @RequestMapping(value="/restSample",method=RequestMethod.GET)
-    
+    @CrossOrigin(origins = "http://spring.example.com:8080", maxAge = 3600)
     public Payload exampleEndpoint() {
         log.info("+++ restSample");  
         return new Payload("test");
