484052667bce9ee67ab230c624f6a72d5715c3fb
Slawomir Borowiec <przodownik@tlen.pl>
2016-01-05 11:11:44 +0100

    git.properties plugin added , build.grade update , actuator+dev boot added

---
+++ src/main/java/pl/java/scalatech/config/HealthConfig.java
@@ -0,0 +1,33 @@
+package pl.java.scalatech.config;
+
+import java.util.Random;
+
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.MBeanExporter;
+
+@Configuration
+public class HealthConfig {
+    @Bean
+    HealthIndicator myHealthIndicator(){
+        return () -> {
+            if(new Random().nextBoolean()){
+                return Health.up().status("ok").build();
+            }
+            return Health.down().status("problem").withDetail("my", "przoodwnik").build();
+        };
+        
+    }
+    
+    @Bean
+    @ExportMetricWriter
+    MetricWriter jmxMetric(MBeanExporter exporter){
+        return new JmxMetricWriter(exporter);
+    }
+    
+}
